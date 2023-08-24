9b7bb9796d327953ed8bd1e4023fc0aee1ef3557
faiq157 <faiq157>
2023-08-24 13:26:33 +0500

    adding Chakra ui

---
+++ src/components/Sidenav.jsx
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const Sidenav = () => {
+  return <Box>Sidenav</Box>;
+};
+
+export default Sidenav;
---
+++ src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Sidenav from "../../components/Sidenav";
+
+const Dashboard = () => {
+  return <Sidenav />;
+};
+
+export default Dashboard;
